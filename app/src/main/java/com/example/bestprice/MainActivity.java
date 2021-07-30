2ea1e30cf0191fdf184943c16616285a135b4957
BlackFire <apoorvagarg9148@gmail.com>
2021-07-30 10:47:20 +0530

    yo

--- app/src/main/java/com/example/bestprice/MainActivity.java
+++ app/src/main/java/com/example/bestprice/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(MainActivity.this, "ljmk",Toast.LENGTH_LONG).show();
+        Toast.makeText(MainActivity.this, "llmk",Toast.LENGTH_LONG).show();
     }
 }
