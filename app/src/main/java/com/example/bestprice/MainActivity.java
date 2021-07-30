c9394cfadc37726d28090e7cbf50543605a0e48f
Oskar Krishna Shrestha <oscarstha@live.com>
2021-07-30 11:28:59 +0545

    check git operation @Oskuda

--- app/src/main/java/com/example/bestprice/MainActivity.java
+++ app/src/main/java/com/example/bestprice/MainActivity.java
@@ -5,12 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
 
+/**
+ * Comment added to mark changes and successful git operations
+ * @Oskuda
+ * TODO: remove later
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(MainActivity.this, "llmk",Toast.LENGTH_LONG).show();
     }
 }
